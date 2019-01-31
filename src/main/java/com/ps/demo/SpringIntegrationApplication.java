ef4881ba35c35357ead19a45d3afe33aa694a4ef
PremRohan <PremRohan@9.85.71.188>
2019-01-31 16:57:56 +0530

    initiate spring integration setup

---
+++ src/main/java/com/ps/demo/SpringIntegrationApplication.java
@@ -0,0 +1,14 @@
+package com.ps.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIntegration1Application.class, args);
+	}
+
+}
+
